9abf57422216fa43590692ad986f5a172dd38336
sasonhe <642139009@qq.com>
2017-10-20 00:38:19 +0800

    es2015

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,43 @@
-
 module.exports = {
     // 入口文件
-    entry : {
-        bundle : './src/index.js'
+    entry: {
+        bundle: './src/index.js'
     },
     // 输出文件、路径
-    output : {
-        path : __dirname + '/build',
-        filename : '[name].js'
+    output: {
+        path: __dirname + '/build',
+        filename: '[name].js'
     },
     // 加载器配置
-    module : {
-        rules : [
-            {
-                test : /\.css$/,
-                use : [
-                    {
-                        loader : 'style-loader'
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
                     },
                     {
-                        loader : 'css-loader'
+                        loader: 'css-loader'
                     }
                 ]
             },
             {
-                test : /\.js$/,
-                use : [
-                    {
-                        loader : 'babel-loader'
-                    }
-                ]
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader'
+                }]
 
             },
             {
                 test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-                use : [
-                    {
-                        loader : 'url-loader'
-                    }
-                ]
+                use: [{
+                    loader: 'url-loader'
+                }]
             },
             {
-                test : /\.vue$/,
-                use : [
-                    {
-                        loader : 'vue-loader'
-                    }
-                ]
+                test: /\.vue$/,
+                use: [{
+                    loader: 'vue-loader'
+                }]
             }
         ]
     }
