a1f3f1874c1372c4c67130195c58b7e7973fd378
sasonhe <642139009@qq.com>
2017-10-23 00:48:50 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
     <yd-tabbar fixed>
-        <yd-tabbar-item title="首页" link="/" active>
+        <yd-tabbar-item title="首页" link="/" exact>
             <yd-icon name="home-outline" slot="icon" size="0.54rem"></yd-icon>
         </yd-tabbar-item>
-        <yd-tabbar-item title="导航1" link="/hello">
+        <yd-tabbar-item title="导航1" link="/hello" exact>
             <yd-icon name="shopcart-outline" slot="icon" size="0.54rem"></yd-icon>
         </yd-tabbar-item>
-        <yd-tabbar-item title="导航2" link="/head">
+        <yd-tabbar-item title="导航2" link="/head" exact>
             <yd-icon name="ucenter-outline" slot="icon" size="0.54rem"></yd-icon>
         </yd-tabbar-item>
         <yd-tabbar-item title="个人中心" link="/lightbox">
@@ -25,5 +25,7 @@ export default {
 }
 </script>
 <style scoped>
-
+.router-link-active {
+    color: rgb(9, 187, 7);
+}
 </style>
