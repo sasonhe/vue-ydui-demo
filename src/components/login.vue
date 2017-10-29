967ab7431c282ef77b7d3b7152baa57639da83e7
sasonhe <642139009@qq.com>
2017-10-30 00:13:28 +0800

    add

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,4 +1,5 @@
 <template>
+<yd-layout>
     <yd-cell-group>
         <yd-cell-item>
             <span slot="left">用户名：</span>
@@ -8,7 +9,24 @@
             <span slot="left">密码：</span>
             <yd-input slot="right" type="password" v-model="input2" placeholder="请输入密码"></yd-input>
         </yd-cell-item>
+        <yd-cell-item>
+            <yd-icon slot="icon" name="phone3" size=".45rem"></yd-icon>
+            <input type="text" slot="right" placeholder="请输入手机号码">
+
+            <!-- ↓↓关键代码是这里↓↓ -->
+            <yd-sendcode slot="right"
+                         v-model="start1"
+                         @click.native="sendCode1"
+                         type="warning"
+            ></yd-sendcode>
+            <!-- ↑↑关键代码是这里↑↑ -->
+
+        </yd-cell-item>
     </yd-cell-group>
+    <yd-button-group>
+        <yd-button size="large" @click.native="openConfrim">提交</yd-button>
+    </yd-button-group>
+</yd-layout>
 </template>
 
 <script type="text/babel">
@@ -16,7 +34,34 @@
         data() {
             return {
                 input1: '',
-                input2: ''
+                input2: '',
+                start1: false
+            }
+        },
+        methods:{
+            openConfrim() {
+                this.$dialog.confirm({
+                    title: '提示信息',
+                    mes: '我有一个小毛驴我从来也不骑！',
+                    opts: () => {
+                        this.$dialog.toast({mes: 'ok', timeout: 1000});
+                    }
+                });
+            },
+            sendCode1() {
+                this.$dialog.loading.open('发送中...');
+                setTimeout(() => {
+
+                    this.start1 = true;
+                    this.$dialog.loading.close();
+
+                    this.$dialog.toast({
+                        mes: '已发送',
+                        icon: 'success',
+                        timeout: 1500
+                    });
+
+                }, 1000);
             }
         }
     }
