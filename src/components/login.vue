ed6e3537a94f378eee23ec8adb99fb1805e194ec
sasonhe <642139009@qq.com>
2017-10-29 23:55:25 +0800

    login

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <yd-cell-group>
+        <yd-cell-item>
+            <span slot="left">用户名：</span>
+            <yd-input slot="right" required v-model="input1" max="20" placeholder="请输入用户名"></yd-input>
+        </yd-cell-item>
+        <yd-cell-item>
+            <span slot="left">密码：</span>
+            <yd-input slot="right" type="password" v-model="input2" placeholder="请输入密码"></yd-input>
+        </yd-cell-item>
+    </yd-cell-group>
+</template>
+
+<script type="text/babel">
+    export default {
+        data() {
+            return {
+                input1: '',
+                input2: ''
+            }
+        }
+    }
+</script>
