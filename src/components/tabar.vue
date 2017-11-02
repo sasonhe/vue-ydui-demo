f51f10f6eaaf4a832ad06c0046e891a463390fb6
sasonhe <642139009@qq.com>
2017-11-02 21:55:45 +0800

    add

--- src/components/tabar.vue
+++ src/components/tabar.vue
@@ -28,9 +28,51 @@
                 </yd-lightbox-txt>
             </yd-lightbox>
         </yd-tab-panel>
-        <yd-tab-panel label="选项二">2222222222</yd-tab-panel>
-        <yd-tab-panel label="选项三">3333333333</yd-tab-panel>
-        <yd-tab-panel label="选项四">4444444444</yd-tab-panel>
+        <yd-tab-panel label="选项二">
+            <yd-list theme="3">
+                <yd-list-item v-for="item, key in list2" :key="key">
+                    <img slot="img" :src="item.img">
+                    <span slot="title">{{item.title}}</span>
+                    <yd-list-other slot="other">
+                        <div>
+                            <span class="demo-list-price"><em>¥</em>{{item.price}}</span>
+                            <span class="demo-list-del-price">¥{{item.w_price}}</span>
+                        </div>
+                        <div>content</div>
+                    </yd-list-other>
+                </yd-list-item>
+            </yd-list>
+        </yd-tab-panel>
+        <yd-tab-panel label="选项三">
+            <yd-list theme="4">
+                <yd-list-item v-for="item, key in list2" :key="key">
+                    <img slot="img" :src="item.img">
+                    <span slot="title">{{item.title}}</span>
+                    <yd-list-other slot="other">
+                        <div>
+                            <span class="demo-list-price"><em>¥</em>{{item.price}}</span>
+                            <span class="demo-list-del-price">¥{{item.w_price}}</span>
+                        </div>
+                        <div>content</div>
+                    </yd-list-other>
+                </yd-list-item>
+            </yd-list>
+        </yd-tab-panel>
+        <yd-tab-panel label="选项四">
+            <yd-list theme="5">
+                <yd-list-item v-for="item, key in list2" :key="key">
+                    <img slot="img" :src="item.img">
+                    <span slot="title">{{item.title}}</span>
+                    <yd-list-other slot="other">
+                        <div>
+                            <span class="demo-list-price"><em>¥</em>{{item.price}}</span>
+                            <span class="demo-list-del-price">¥{{item.w_price}}</span>
+                        </div>
+                        <div>content</div>
+                    </yd-list-other>
+                </yd-list-item>
+            </yd-list>
+        </yd-tab-panel>
     </yd-tab>
 </div>
 </template>
@@ -62,6 +104,43 @@ export default {
                 {
                     src: 'http://static.ydcss.com/uploads/lightbox/meizu_s6.jpg',
                     original: 'http://static.ydcss.com/uploads/lightbox/meizu_6.jpg'
+                }
+            ],
+            list2: [{
+                    img: "//img1.shikee.com/try/2016/06/23/14381920926024616259.jpg",
+                    title: "标题111标题标题标题标题",
+                    price: 156.23,
+                    w_price: 89.36
+                },
+                {
+                    img: "//img1.shikee.com/try/2016/06/21/10172020923917672923.jpg",
+                    title: "标题222标题标题标题标题",
+                    price: 256.23,
+                    w_price: 89.36
+                },
+                {
+                    img: "//img1.shikee.com/try/2016/06/23/15395220917905380014.jpg",
+                    title: "标题333标题标题标题标题",
+                    price: 356.23,
+                    w_price: 89.36
+                },
+                {
+                    img: "//img1.shikee.com/try/2016/06/25/14244120933639105658.jpg",
+                    title: "标题444标题标题标题标题",
+                    price: 456.23,
+                    w_price: 89.36
+                },
+                {
+                    img: "//img1.shikee.com/try/2016/06/26/12365720933909085511.jpg",
+                    title: "标题555标题标题标题标题",
+                    price: 556.23,
+                    w_price: 89.36
+                },
+                {
+                    img: "//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg",
+                    title: "标题666标题标题标题标题",
+                    price: 656.23,
+                    w_price: 89.36
                 }
             ]
         }
