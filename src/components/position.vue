4489b6ab44d919fdf2f141d503aa96a38c1668fc
sasonhe <642139009@qq.com>
2017-11-03 18:17:10 +0800

    add 高德地图

---
+++ src/components/position.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="amap-wrapper">
+      <el-amap :vid="'amap-vue'"></el-amap>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.amap-wrapper {
+  width: 100%;
+  height: 600px;
+}
+</style>
