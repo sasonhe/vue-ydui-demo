b7076ff92e931cb9f4a10d6f5cab24e0ed6daeda
sasonhe <642139009@qq.com>
2017-11-07 15:49:30 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,18 +3,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import App from './App.vue'
-// import AMap from 'vue-amap'
 
 import YDUI from 'vue-ydui'
 import 'vue-ydui/dist/ydui.rem.css'
 import 'vue-ydui/dist/ydui.flexible.js'
 // 引入路由配置
 import routes from './router.config'
-//全局样式重置
-// import reset from './common/reset.css'
-//
+
 Vue.use(YDUI)
-// Vue.use(AMap)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
@@ -22,13 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     typeof FastClick === 'function' && FastClick.attach(document.body);
 }, false)
 
-// AMap.initAMapApiLoader({
-//   key: 'b7d8182509200b3c7bc6e08c58043dab',
-//   plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
-// })
-
 var router = new VueRouter({
-    // mode: 'history',
     routes
 })
 
