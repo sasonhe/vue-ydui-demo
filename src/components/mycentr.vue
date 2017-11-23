e57a3a851e92d01bdfae95eaa0430a8c29d2f3d7
sasonhe <642139009@qq.com>
2017-11-23 10:52:55 +0800

    delete login

--- src/components/mycentr.vue
+++ src/components/mycentr.vue
@@ -2,14 +2,14 @@
 <div>
     <h1>Hello World</h1>
     <yd-countdown time="2018/01/01 00:00:00" class="time"></yd-countdown>
-    <login></login>
+    <!-- <login></login> -->
 </div>
 </template>
 <script>
-import login from './login.vue'
+// import login from './login.vue'
 export default {
     components: {
-        login
+        // login
     }
 }
 </script>
